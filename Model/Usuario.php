2713da52344e6a050324fe2896052fe3c2a449dc
Alessandro dos Santos DESKTOP <professor_alessandro@hotmail.com>
2019-11-01 00:05:15 -0300

    ´Primeiro Commit

---
+++ Model/Usuario.php
@@ -0,0 +1,59 @@
+﻿<?php
+namespace Chayds
+{
+	class Usuario
+	{
+		//ATRIBUTOS
+		private $id;
+		private $nome;
+		private $email;
+		private $senha;
+		//ATRIBUTOS
+
+		//CONSTRUTORES
+		public function __construct(id = "",$nome = "", $email = "",  $senha= "")
+		{
+			$this->id = $id;
+			$this->nome = $nome;
+			$this->email = $email;
+			$this->senha = $senha;
+		}//CONSTRUTORES
+
+		//PROPRIEDADES
+		public function setId($valor)
+		{
+			$this->id = $valor;
+		}
+		public function getId()
+		{
+			return $this->id ;
+		}
+		public function setNome($valor)
+		{
+			$this->nome = $valor;
+		}
+		public function getNome()
+		{
+			return $this->nome ;
+		}
+		public function setEmail($valor)
+		{
+			$this->email = $valor;
+		}
+		public function getEmail()
+		{
+			return $this->email ;
+		}
+		public function setSenha($valor)
+		{
+			$this->senha = $valor;
+		}
+		public function getSenha()
+		{
+			return $this->senha ;
+		}
+		//PROPRIEDADES
+	}//CLASS
+}//NAMESPACE
+
+?>
---
+++ View/Usuario.php
@@ -0,0 +1,47 @@
+﻿<?php include_once("../../Conexao/Conexao.php"); ?>
+<?php include_once("../Model/Usuario.php"); ?>
+<?php include_once("../Model/DAOUsuario.php"); ?>
+<?php
+	class Usuario
+	{
+		//INICIALIZA A CONEXAO
+		$conexao = new Conexao();
+		
+		//INICIALIZA O CONSTRUTOR DO DAO COM A NOVA CONEXAO
+		$daoUsuario = new DAOUsuario($conexao);
+		
+		//O OBJETO QUE OS METODOS VAO USAR PARA FAZER AS OPEAÇÕES NO BANCO DE DADOS
+		$usuario = new Usuario();
+		
+		INSERIR O USUARIO
+		$daoUsuario->inserirUsuario($usuario);
+		
+		//INICIALIZA A CONEXAO NOVAMENTE POES CADA METODO TEM NO FINAL UMA LINHA PRA FECHAR A CONEXAO
+		$conexao = new Conexao();
+		
+		//INICIALIZA O CONSTRUTOR DO DAO COM A NOVA CONEXAO
+		$daoUsuario = new DAOUsuario($conexao);
+		
+		//LOCALIZA O USUARIO UM NO BANCO DE DADOS
+		$usuario = localizarUsuario(1)
+		
+		//INICIALIZA A CONEXAO NOVAMENTE POES CADA METODO TEM NO FINAL UMA LINHA PRA FECHAR A CONEXAO
+		$conexao = new Conexao();
+		
+		//INICIALIZA O CONSTRUTOR DO DAO COM A NOVA CONEXAO
+		$daoUsuario = new DAOUsuario($conexao);
+		
+		//altera o usuario
+		$daoUsuario->alterarUsuario($usuario);
+		
+		//INICIALIZA A CONEXAO NOVAMENTE POES CADA METODO TEM NO FINAL UMA LINHA PRA FECHAR A CONEXAO
+		$conexao = new Conexao();
+		
+		//INICIALIZA O CONSTRUTOR DO DAO COM A NOVA CONEXAO
+		$daoUsuario = new DAOUsuario($conexao);
+		
+		//EXCLUI O USUARIO 1
+		$daoUsuario->excluirUsuario(1);
+		
+	}//CLASS
+?>
